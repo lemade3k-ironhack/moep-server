1bfbf4ef85c73ba5cb4fda08ba275f58fce445ac
Anna Tielke <anna_tielke@gmx.de>
2021-05-13 17:58:49 +0200

    changed Usermodel, Created authRoutes, middleware in app.js

--- models/User.model.js
+++ models/User.model.js
@@ -1,38 +1,33 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      require: true,
-      minLength: 8,
-    },
-    passwordConfirmation: {
-      type: String,
-      require: true,
-      minLength: 8,
-    },
-    role: {
-      type: String,
-      enum: ["admin", "user"],
-      default: "user",
-    },
-    // a users favorites
-    concerts: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Concert",
+   {
+      name: {
+         type: String,
+         required: true,
+         unique: true,
       },
-    ],
-  },
-  {
-    timestamps: true,
-  }
+      password: {
+         type: String,
+         require: true,
+         minLength: 8,
+      },
+      role: {
+         type: String,
+         enum: ["admin", "user"],
+         default: "user",
+      },
+      // a users favorites
+      concerts: [
+         {
+            type: Schema.Types.ObjectId,
+            ref: "Concert",
+         },
+      ],
+   },
+   {
+      timestamps: true,
+   }
 );
 
 const User = model("User", userSchema);
