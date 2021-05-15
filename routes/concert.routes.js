109d6d783ce8dfe48462f93274c38d86c50ddaba
lemade <made@honkgong.info>
2021-05-15 10:45:28 +0200

    Sort concerts by bandname for lineup

--- routes/concert.routes.js
+++ routes/concert.routes.js
@@ -1,20 +1,22 @@
 const express = require("express");
-const { routes } = require("../app");
 const router = express.Router();
 let Concert = require("../models/Concert.model");
 let Stage = require("../models/Stage.model");
 
 router.get("/concerts", (req, res) => {
-   Concert.find()
-      .then((concerts) => {
-         res.status(200).json(concerts);
-      })
-      .catch((err) => {
-         res.status(500).json({
-            errorMessage: "Something went wrong. Please try again",
-            message: err,
-         });
+  Concert.find()
+    .then((concerts) => {
+      sorted = concerts.sort((a, b) => {
+        a.bandname > b.bandname ? 1 : b.bandname > a.bandname ? -1 : 0;
       });
+      res.status(200).json(sorted);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        errorMessage: "Something went wrong. Please try again",
+        message: err,
+      });
+    });
 });
 
 router.get("/stages/:stageId/concerts", (req, res) => {
