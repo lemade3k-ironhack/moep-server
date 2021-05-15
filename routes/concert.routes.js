e02a655bfe280e5f85acb3354050350cb454dbeb
lemade <made@honkgong.info>
2021-05-15 10:46:24 +0200

    Add concert create route

--- routes/concert.routes.js
+++ routes/concert.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 let Concert = require("../models/Concert.model");
 let Stage = require("../models/Stage.model");
 
+// Get upcoming shows
 router.get("/upcoming", (req, res) => {
   Concert.find()
     .then((concerts) => {
@@ -22,6 +23,7 @@ router.get("/upcoming", (req, res) => {
     });
 });
 
+// Get lineup (all concerts/bands)
 router.get("/concerts", (req, res) => {
   Concert.find()
     .then((concerts) => {
@@ -38,33 +40,84 @@ router.get("/concerts", (req, res) => {
     });
 });
 
+// Get all concerts of one stage
 router.get("/stages/:stageId/concerts", (req, res) => {
-   const { stageId } = req.params;
-   Stage.findById(stageId)
-      .populate("concerts")
-      .then((stage) => {
-         res.status(200).json(stage.concerts);
-      })
-      .catch((err) => {
-         res.status(500).json({
-            errorMessage: "Something went wrong. Please try again",
-            message: err,
-         });
+  const { stageId } = req.params;
+  Stage.findById(stageId)
+    .populate("concerts")
+    .then((stage) => {
+      res.status(200).json(stage.concerts);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        errorMessage: "Something went wrong. Please try again",
+        message: err,
       });
+    });
 });
 
+// custom middleware to validate user input
+const isFilledIn = (req, res, next) => {
+  const { bandname, day, starttime, endtime } = req.body;
+
+  if (!bandname || !day || !starttime || !endtime) {
+    res.status(500).json({
+      errorMessage: "Please enter all fields",
+    });
+    return;
+  } else {
+    next();
+  }
+};
+
+// Create concert
+router.post("/stages/:stageId/concerts/create", isFilledIn, (req, res) => {
+  const { stageId } = req.params;
+  const { bandname, day, starttime, endtime, description, image } = req.body;
+  const img = image ? image : "/concertDummy.png";
+
+  Concert.create({
+    bandname,
+    day,
+    starttime,
+    endtime,
+    description,
+    image: img,
+    stage: stageId,
+  })
+    .then((concert) => {
+      Stage.findByIdAndUpdate(stageId, {
+        $push: { concerts: concert._id },
+      }).then(() => res.status(202).json(concert));
+    })
+    .catch((err) => {
+      if (err.code === 11000) {
+        res.status(500).json({
+          errorMessage: "Band name already taken",
+          message: err,
+        });
+      } else {
+        res.status(500).json({
+          errorMessage: "Couldn't create concert! Please try again.",
+          message: err,
+        });
+      }
+    });
+});
+
+// Get concert details
 router.get("/concerts/:concertId", (req, res) => {
-   const { concertId } = req.params;
-   Concert.findById(concertId)
-      .then((concert) => {
-         res.status(200).json(concert);
-      })
-      .catch((err) => {
-         res.status(500).json({
-            errorMessage: "Something went wrong. Please try again",
-            message: err,
-         });
+  const { concertId } = req.params;
+  Concert.findById(concertId)
+    .then((concert) => {
+      res.status(200).json(concert);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        errorMessage: "Something went wrong. Please try again",
+        message: err,
       });
+    });
 });
 
 module.exports = router;
